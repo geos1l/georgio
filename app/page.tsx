acc6b259b0a81008e4829ecfb97686c851f7fbbd
Georgio <quantumpi14x@gmail.com>
2026-04-06 23:20:42 -0400

    removed email part

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero";
 import ProjectsSection from "@/components/ProjectsSection";
-import ContactCTA from "@/components/ContactCTA";
 import Divider from "@/components/Divider";
 import Footer from "@/components/Footer";
 
@@ -15,11 +14,7 @@ export default function Home() {
       <div className="max-w-4xl mx-auto px-6">
         <Divider />
       </div>
-      <ContactCTA />
-      <div className="max-w-4xl mx-auto px-6">
-        <Divider />
-      </div>
-      <Footer />
+<Footer />
     </>
   );
 }
